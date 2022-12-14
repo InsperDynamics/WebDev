e8888063b9b101a5b8ae9039b85e3b0796e95988
Antônio A E Martins <antonioaem@icloud.com>
2022-12-14 14:58:33 -0300

    Esqueleto de Formulario Processo Seletivo

--- Dynamics/usuarios/models.py
+++ Dynamics/usuarios/models.py
@@ -1,3 +1,31 @@
+from email import message
+from email.mime import image
+from email.policy import default
 from django.db import models
+from django.contrib.auth.models import User
+from psycopg2 import cursor
+from sqlalchemy import null
+import datetime
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(max_length=100, blank=True)
+    image = models.ImageField(default='default.jpg')
+
+    def __str__(self):
+        return self.username
+
+class Register_token(models.Model):
+    token = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.token
+
+class ProcessoSeletivo(models.Model):
+    nome = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    curso = models.CharField(max_length=50)
+    semestre = models.IntegerField(max_length=50)
+    telefone = models.CharField(max_length=50)
--- Dynamics/usuarios/views.py
+++ Dynamics/usuarios/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from tokenize import Token
+from unicodedata import name
+from urllib import response
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.utils.datastructures import MultiValueDictKeyError
+from .models import *
+from django.contrib.auth import login
+from .forms import *
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator
+import requests
+from rest_framework.response import Response
+import json
+import datetime
+from django.contrib.auth.hashers import make_password
+from django.http import FileResponse, HttpResponse, HttpResponseNotFound, JsonResponse
+from django.conf import settings
 
 # Create your views here.
+
+
+def index(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        curso = request.POST.get('curso')
+        semestre = request.POST.get('semestre')
+        telefone = request.POST.get('telefone')
+
+        PS = ProcessoSeletivo(nome = nome, email=email, curso=curso, semestre=semestre, telefone=telefone)
+        PS.save()
+    return render(request, 'index.html')
+        
